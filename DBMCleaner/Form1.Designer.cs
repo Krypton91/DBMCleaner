0b8e714c6546d9e297b60bbe7ec2a30a8c4390e7
Krypton91 <krypton1@gmx.at>
2020-09-03 14:19:10 +0200

    Initial commit

---
+++ DBMCleaner/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace DBMCleaner
+{
+    partial class Cleaner
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cleaner));
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button12 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(212, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Files:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Crimson;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(644, 37);
+            this.panel1.TabIndex = 3;
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Webdings", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(616, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "r";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Webdings", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(584, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 20);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "0";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Silver;
+            this.label5.Location = new System.Drawing.Point(3, 5);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(343, 26);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "DEUTSCHE BOHRMASCHINE LOG CLEANER";
+            // 
+            // button12
+            // 
+            this.button12.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button12.ForeColor = System.Drawing.Color.White;
+            this.button12.Location = new System.Drawing.Point(215, 203);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(181, 24);
+            this.button12.TabIndex = 6;
+            this.button12.Text = "Clean";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(10, 270);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Info:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(44, 270);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(435, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "This program is Part of DeutscheBohrmaschine, and it will clean your Log Folder f" +
+    "rom Dayz!";
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(215, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 24);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Send Logs Only";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::DBMCleaner.Properties.Resources._1024px_GitHub_logo_2013_svg;
+            this.pictureBox2.Location = new System.Drawing.Point(504, 245);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(128, 38);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DBMCleaner.Properties.Resources.DeutscheBohrmaschineLOGO;
+            this.pictureBox1.Location = new System.Drawing.Point(88, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(480, 121);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Cleaner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(644, 292);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Cleaner";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DeutscheBohrmaschine Cleaner";
+            this.Load += new System.EventHandler(this.Cleaner_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Cleaner_MouseDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ DBMCleaner/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBMCleaner
+{
+    public partial class Cleaner : Form
+    {
+        private List<string> logs = new List<string>();
+        static string path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\DayZ\\";
+        static string zipPath = path + Environment.UserName + "`s Dayz Logs.zip";
+        public static int x;
+        public static int y;
+        public bool m_isGenerated;
+        public const string Prefix = "DBM Cleaner";
+        public static System.Drawing.Point newpoint = new System.Drawing.Point();
+        public Cleaner()
+        {
+            InitializeComponent();
+        }
+        bool IsZipFileGenerated() 
+        {
+            return m_isGenerated;
+        }
+        internal void SafeLogs()
+        {
+            if (File.Exists(zipPath)) 
+            {
+                File.Delete(zipPath);
+            }
+            var zip = ZipFile.Open(zipPath, ZipArchiveMode.Create);
+            foreach (string fileName in logs)
+            {
+                if (fileName.Contains(".log") || fileName.Contains(".log") || fileName.Contains(".mdmp"))
+                {
+                    zip.CreateEntryFromFile(fileName, Path.GetFileName(fileName), CompressionLevel.Optimal);
+                }
+            }
+            m_isGenerated = true;
+            zip.Dispose();
+        }
+        private void Cleaner_Load(object sender, EventArgs e)
+        {
+            DetectLogs();
+        }
+        internal List<string> fileList = new List<string>
+        {
+            "log",
+            "rpt",
+            "ADM",
+            "mdmp",
+        };
+        internal string ConvertBytes(float Size, int R)
+        {
+            float num = Size / 1024f;
+            if (num < 1f)
+            {
+                switch (R)
+                {
+                    case 0:
+                        return string.Format("{0:0.00} byte", Size);
+                    case 1:
+                        return string.Format("{0:0.00} kb", Size);
+                    case 2:
+                        return string.Format("{0:0.00} mb", Size);
+                    case 3:
+                        return string.Format("{0:0.00} gb", Size);
+                    case 4:
+                        return string.Format("{0:0.00} tb", Size);
+                }
+            }
+            return ConvertBytes(num, ++R);
+        }
+        internal string BytesToString(float Size)
+        {
+            return ConvertBytes(Size, 0);
+        }
+        internal async Task DetectLogs() 
+        {
+            long fileSize = 0L;
+            foreach (string str in fileList)
+            {
+                logs.AddRange(Directory.GetFiles(path, "*." + str));
+            }
+            foreach (string fileName in logs)
+            {
+                fileSize += new FileInfo(fileName).Length;
+            }
+            await Task.Delay(1);
+            label2.Text = string.Format("Found: {0} files with a size of: {1}.", logs.Count, BytesToString(fileSize));
+        }
+        internal void StartCleaning() 
+        {
+            foreach (string fileName in logs)
+            {
+                try
+                {
+                    new FileInfo(fileName).Delete();
+                    DialogResult dialogResult = MessageBox.Show("All Dayz Log Files has been deleted!\nYou want to exit now ?", Prefix, MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        this.Close();
+                    }
+                    else if (dialogResult == DialogResult.No)
+                    {
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            MessageBox.Show("No Logs to delete DBM Cleaner Closing now!", Prefix);
+            this.Close();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+        internal void SendWebReq() 
+        {
+            try
+            {
+                if (IsZipFileGenerated() == true)
+                {
+                    System.Net.WebClient Client = new System.Net.WebClient();
+
+                    Client.Headers.Add("Content-Type", "binary/octet-stream");
+
+                    byte[] result = Client.UploadFile("https://report.deutschebohrmaschine.de/upload.php", "POST", zipPath);
+
+                    string s = System.Text.Encoding.UTF8.GetString(result, 0, result.Length);
+                }
+                else 
+                {
+                    MessageBox.Show("ZIP NOT GENERATED");
+                    SafeLogs();
+                }
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+            base.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("WARNING \n Can i send your crash logs to Krypton?\n Please Accept this with yes\nIt will only send .log files in Dayz Folder!\nWhen you click no it will clean you PC!", Prefix, MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                SendLogsToKrypton();
+                StartCleaning();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                StartCleaning();
+            }
+        }
+
+        private void Cleaner_MouseDown(object sender, MouseEventArgs e)
+        {
+            Cleaner.x = Control.MousePosition.X - base.Location.X;
+            Cleaner.y = Control.MousePosition.Y - base.Location.Y;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Cleaner.newpoint = Control.MousePosition;
+                Cleaner.newpoint.X -= Cleaner.x;
+                Cleaner.newpoint.Y -= Cleaner.y;
+                base.Location = Cleaner.newpoint;
+            }
+        }
+        internal void SendLogsToKrypton() 
+        {
+            SafeLogs();
+            SendWebReq();
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("WARNING \n This Option Uploads your Logs!!\n Your Logs are keep private we use it to solve the client crashes!\nIt will only send .log and dump files in Dayz Folder!\nClick Yes to send the Logs!", Prefix, MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                SendLogsToKrypton();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Process.Start("");
+        }
+    }
+}
